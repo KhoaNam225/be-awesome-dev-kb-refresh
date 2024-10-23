a424d58959ee1043e20b5f9227798af52646ff5d
Nam Pham <pknam25@gmail.com>
2024-10-23 20:58:29 +0800

    Removed the summary content generation part and updated the KB and Data source id

--- be-awesome-dev-kb-refresh-function/function/app.py
+++ be-awesome-dev-kb-refresh-function/function/app.py
@@ -12,8 +12,8 @@ from agent.post_summariser import summariser_chain
 def lambda_handler(event, context):
     source_posts_bucket = "be-awesome-dev-posts"
     destination_kb_bucket = "be-awesome-dev-chatbot-knowledge-base"
-    knowledge_base_id = "5GRDEUTEQC"
-    data_source_id = "9KNFRIOVZ7"
+    knowledge_base_id = "EZS73QFPKR"
+    data_source_id = "R5X1STDKV5"
 
     # Sync content between source posts bucket and the KB bucket
     sync_buckets_content(
@@ -21,21 +21,21 @@ def lambda_handler(event, context):
     )
 
     # Create the summary of all posts
-    posts = get_posts_from_bucket(bucket=source_posts_bucket)
-    summaries = generate_summaries(posts=posts, summary_chain=summariser_chain)
+    # posts = get_posts_from_bucket(bucket=source_posts_bucket)
+    # summaries = generate_summaries(posts=posts, summary_chain=summariser_chain)
 
-    print("Generating summary.md file")
-    summary_output_file = "/tmp/summary.md"
-    write_posts_summary_content(file_path=summary_output_file, summaries=summaries)
+    # print("Generating summary.md file")
+    # summary_output_file = "/tmp/summary.md"
+    # write_posts_summary_content(file_path=summary_output_file, summaries=summaries)
 
     # Upload summary to KB bucket
-    upload_object_to_bucket(
-        bucket_name=destination_kb_bucket,
-        source_file_path=summary_output_file,
-        bucket_file_path="summary.md",
-    )
+    # upload_object_to_bucket(
+    #     bucket_name=destination_kb_bucket,
+    #     source_file_path=summary_output_file,
+    #     bucket_file_path="summary.md",
+    # )
 
-    print("Uploaded summary file to KB bucket")
+    # print("Uploaded summary file to KB bucket")
 
     # Sync data from s3 bucket to bedrock knowledge base
     print("Starting syncing the knowledge base bucket with bedrock")
